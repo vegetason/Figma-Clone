a7dd6fa897b6da15134d4379a034f02a81da8aa0
vegetason <irakozepaulin12@gmail.com>
2025-07-12 14:32:48 +0200

    Add Left side bar design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+"use client"
+//ts-ignore
+import LeftSideBar from "@/components/LeftSideBar";
+
 
 export default function Home() {
+
   return (
-    <></>
+    <div className="flexw-full h-[100vh]">
+      <LeftSideBar></LeftSideBar>
+    </div>
   );
 }
